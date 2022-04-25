33001743e96c1ff23791c1416e960e232de832f6
Ilyakoc <ilakochenko@yandex.ru>
2022-04-25 20:30:35 +0700

    site

---
+++ protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * DB connection config
+ */
+return array(
+    'connectionString' => 'mysql:host=localhost;dbname=cl30234_instrum',
+    'username' => 'cl30234_instrum',
+    'password' => 'M6wMnSxM',
+);
